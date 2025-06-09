ecec8c863531159e35938db57a60a22c3634813b
pmgespina <pmgespina@gmail.com>
2025-06-09 09:50:24 +0000

    Typedomain and typerange columns added to object property tables

--- src/main/java/table/impl/RulesConsProp.java
+++ src/main/java/table/impl/RulesConsProp.java
@@ -1,11 +1,12 @@
 package table.impl;
 
-import com.datastax.oss.driver.api.core.type.DataTypes;
-import database.Database;
-import table.CassandraTable;
-
 import java.io.Serializable;
 import java.text.MessageFormat;
+
+import com.datastax.oss.driver.api.core.type.DataTypes;
+
+import database.Database;
+import table.CassandraTable;
 
 public class RulesConsProp extends CassandraTable {
 
@@ -19,6 +20,8 @@ public class RulesConsProp extends CassandraTable {
         this.Columns.put("domain", DataTypes.TEXT);
         this.Columns.put("prop", DataTypes.TEXT);
         this.Columns.put("range", DataTypes.TEXT);
+        this.Columns.put("typedomain", DataTypes.TEXT);
+        this.Columns.put("typerange", DataTypes.TEXT);
     }
 
     public static class Row implements Serializable {
@@ -28,13 +31,17 @@ public class RulesConsProp extends CassandraTable {
         private final String domain;
         private final String prop;
         private final String range;
+        private final String typedomain;
+        private final String typerange;
 
-        public Row(Integer ruleid, Integer num, String domain, String prop, String range) {
+        public Row(Integer ruleid, Integer num, String domain, String prop, String range, String typedomain, String typerange) {
             this.ruleid = ruleid;
             this.num = num;
             this.domain = domain;
             this.prop = prop;
             this.range = range;
+            this.typedomain = typedomain;
+            this.typerange = typerange; 
         }
 
         public Integer getRuleId() {
@@ -57,9 +64,18 @@ public class RulesConsProp extends CassandraTable {
             return range;
         }
 
+        public String getTypeDomain() {
+            return typedomain;
+        }
+
+        public String getTypeRange() {
+            return typerange;
+        }
+
         @Override
         public String toString() {
-            return MessageFormat.format("RulesConsProp'{'ruleid={0}, num={1}, domain={2}, prop={3}, range={4}'}'", ruleid, num, domain, prop, range);
+            return MessageFormat.format("RulesConsProp'{'ruleid={0}, num={1}, domain={2}, prop={3}, range={4}, typedomain={5}, typerange={6}'}'", 
+                    ruleid, num, domain, prop, range, typedomain, typerange);
         }
     }
 
